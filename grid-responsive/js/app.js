68ac6aa19932da93ed28673070a7091fbcba6e89
art_server <polito.abs@gmail.com>
2021-06-11 09:33:38 +0200

    updated version for grid responsive page

--- grid-responsive/js/app.js
+++ grid-responsive/js/app.js
@@ -1,8 +1,55 @@
-var navbar = document.querySelector('navbar');
-//var shrink = navbar.offsetTop;
+const navbar = document.querySelector('.main-header');
 
-window.onscroll = function () {
-	if (window.pageYOffset >= 10) {
-		navbar.classList.add('shrink')
+window.onscroll = function (){
+	if (window.pageYOffset >= 2){
+		navbar.classList.add('fixed-bar');
+		navbar.style.backgroundColor = `rgba(240, 240, 240,.8)`;
+	} else {
+		navbar.classList.remove('fixed-bar');
+		navbar.style.backgroundColor = `rgb(240, 240, 240)`;
 	}
 }
+
+var slideIndex = 0;
+showDiapo(slideIndex);
+
+// Next & previous buttons
+var btn_left = document.getElementById('btn_left');
+var btn_right = document.getElementById('btn_right');
+
+btn_left.onclick = function (){avanzaDiapo(-1)};
+btn_right.onclick = function (){avanzaDiapo(1)};
+
+function avanzaDiapo(n) {
+  showDiapo(slideIndex += n);
+}
+
+// Dots controls
+function currentDiapo(n) {
+  showDiapo(slideIndex = n);
+}
+
+// Carousel function: loops over the elements.
+function showDiapo(n) {
+  // select all elements with class "slide" and then "dot"
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+
+  // return to first slide if going next from last one
+  if (n > slides.length-1) {slideIndex = 0}
+  // got to last slide if coming back from first one	
+  if (n < 0) {slideIndex = slides.length -1}
+  // loop over the slides to hide them	
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  // loop over the dots to switch them to inactive
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  // display the slide at current index
+  slides[slideIndex].style.display = "grid";
+  // display the slide for the corresponding active dot
+  dots[slideIndex].className += " active";
+} 
